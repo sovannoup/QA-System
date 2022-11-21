576f9467e911309582f1417d9980bc559fb0df85
sovannoup <noupsovan18@kit.edu.kh>
2022-11-21 17:21:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import './App.css';
+import HomePage from './screens/HomePage';
+import AskQuestionPage from './screens/AskQuestionPage';
+import UserProfilePage from './screens/UserProfile';
+import ViewQuestionPage from './screens/ViewQuestionPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/ask-question" element={<AskQuestionPage />} />
+          <Route exact path="/profile" element={<UserProfilePage />} />
+          <Route exact path="/view-question" element={<ViewQuestionPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
